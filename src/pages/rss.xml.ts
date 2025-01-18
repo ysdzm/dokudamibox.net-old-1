2cb2bc906b1c84d3184700577a0658bd7a3c2ceb
dokudami <69578376+ysdzm@users.noreply.github.com>
2025-01-18 18:37:17 +0900

    fix: 新しいテーマをとりあえず導入

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,30 @@
 import rss from "@astrojs/rss";
-import { siteConfig } from "@/site-config";
-import { getAllPosts } from "@/data/post";
+import { getCollection } from "astro:content";
+import { HOME } from "@consts";
 
-export const GET = async () => {
-	const posts = await getAllPosts();
+type Context = {
+  site: string
+}
 
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.publishDate,
-			link: `posts/${post.slug}`,
-		})),
-	});
-};
+export async function GET(context: Context) {
+  const blog = (await getCollection("blog"))
+  .filter(post => !post.data.draft);
+
+  const projects = (await getCollection("projects"))
+    .filter(project => !project.data.draft);
+
+  const items = [...blog, ...projects]
+    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+
+  return rss({
+    title: HOME.TITLE,
+    description: HOME.DESCRIPTION,
+    site: context.site,
+    items: items.map((item) => ({
+      title: item.data.title,
+      description: item.data.description,
+      pubDate: item.data.date,
+      link: `/${item.collection}/${item.slug}/`,
+    })),
+  });
+}
